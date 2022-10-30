c184dd0a0931de25bb10c6aca242bcb343bd9c6f
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2022-10-30 22:49:07 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,31 @@
-REPOSITORY=/home/ubuntu/app
-cd $REPOSITORY
+#!/usr/bin/env bash
 
-APP_NAME=api #1
-JAR_NAME=$(ls $REPOSITORY/api/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/api/build/libs/$JAR_NAME
+REPOSITORY=/home/ubuntu/app/api/build/libs
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-if [ -z $CURRENT_PID ] #2
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ec2-user/app/build/libs/*.jar > /dev/null 2> /dev/null < /dev/null &
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
